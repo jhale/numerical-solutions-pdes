7f4168d97675af5f87f870911638a84fc6b7e9aa
Jack S. Hale <mail@jackhale.co.uk>
2025-02-17 17:14:09 +0100

    Moving in the right direction.

--- chapters/part1.py
+++ chapters/part1.py
@@ -15,6 +15,43 @@
 
 # %% [markdown]
 # # The Galerkin finite element method
-
-# %%
-import numpy as np
+#
+# In this notebook we show the creation of a simple one-dimensional Galerkin
+# finite element code for the following reaction-diffusion type partial
+# differential equation.
+#
+# The notebook is not complete - your task will be to complete the missing
+# parts and submit the completed notebook as the first part of the coursework. 
+#
+# Find $u : [0, 1] \to \mathbb{R}$ such that
+#
+# $$
+# - \nabla u + k^2 u = f,
+# $$
+#
+# with Dirichlet-type boundary conditions
+# 
+# $$
+# u(0) = 0, \quad u(1) = 0. 
+# $$
+#
+# ## Exercise 1
+# 
+# Comment on the following aspects of this PDE:
+#
+# 1. Linear or non-linear, and why?
+# 2. For $k^2 > 0$, elliptic, parabolic or hyperbolic? 
+# 
+# %% [markdown]
+# Write your answer here.
+#
+# %% [markdown]
+# ## Exercise 2
+# 
+# We would like to *verify* that our finite element method is working as we
+# would expect (more on this later). One straightforward way to do this is via
+# the method of manufactured solutions - we will invent a solution which satisfies
+# the Dirichlet boundary conditions
+#
+# %% [markdown]
+# Write your answer here.
