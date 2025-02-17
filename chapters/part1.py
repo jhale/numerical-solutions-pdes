ca6045d9e4a9658efe9242c88cb111e1ac439ad8
Jack S. Hale <mail@jackhale.co.uk>
2025-02-17 17:33:31 +0100

    Start code.

--- chapters/part1.py
+++ chapters/part1.py
@@ -20,8 +20,6 @@
 # finite element code for the following reaction-diffusion type partial
 # differential equation.
 #
-# The notebook is not complete - your task will be to complete the missing
-# parts and submit the completed notebook as the first part of the coursework.
 #
 # Find $u : [0, 1] \to \mathbb{R}$ such that
 #
@@ -35,25 +33,42 @@
 # u(0) = 0, \quad u(1) = 0.
 # $$
 #
+# *The notebook is not complete - your task will be to complete the missing
+# parts and submit it as the first part of the coursework.*
+#
 # ## Exercise 1
 #
 # Comment on the following aspects of this PDE:
 #
 # 1. Linear or non-linear, and why?
 # 2. For $k^2 > 0$, elliptic, parabolic or hyperbolic?
-#
+
 # %% [markdown]
-# Write your answer here.
+# *Answer*
 #
+# Write your answer using Markdown here.
+
 # %% [markdown]
 # ## Exercise 2
 #
-# We would like to *verify* that our finite element method is working as we
-# would expect (more on this later). One straightforward way to do this is via
-# the method of manufactured solutions - we will invent a solution which satisfies
-# the Dirichlet boundary conditions
-#
+# Eventually, we would like to *verify* that our finite element method is
+# working as we would expect (more on this later). One straightforward way to
+# do this is via the method of manufactured solutions - we will invent a
+# solution $u_{\mathrm{exact}}$ that satisfies the Dirichlet boundary
+# conditions, then derive $f$. We will then solve this problem using our
+# finite element code, and check that it converges at the correct rate -
+# this gives us a reasonable guarantee that
+
 # %% [markdown]
-# Write your answer here.
+# *Answer*
+#
+# Write your answer using Markdown here.
 
 # %%
+import numpy as np
+
+
+def generate_mesh(n):
+    cell_vertex_connectivity = np.zeros()
+
+    return cell_vertex_connectivity
