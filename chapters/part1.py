e5f3401cf8bfd6c4ed519a795fc1320ce28aeff8
Jack S. Hale <mail@jackhale.co.uk>
2025-02-19 16:16:10 +0100

    Reflow.

--- chapters/part1.py
+++ chapters/part1.py
@@ -68,9 +68,10 @@
 # method of manufactured solutions. We will invent a solution
 # $u_{\mathrm{exact}}$ that satisfies the Dirichlet boundary conditions, then
 # derive $f$ via substitution into the strong form of the PDE. On a sequence of
-# finer and finer meshes, we will solve our PDE using our finite element code and check that our numerical
-# solution converges to $u_{\mathrm{exact}}$ at the *correct* rate. If it does,
-# then it is likely we have a correct implementation.
+# finer and finer meshes, we will solve our PDE using our finite element code
+# and check that our numerical solution converges to $u_{\mathrm{exact}}$ at
+# the *correct* rate. If it does, then it is likely we have a correct
+# implementation.
 #
 # Let $u_{\mathrm{exact}} = \sin(cx)$ with $c$ an unknown positive constant.
 # Find the values for $c$ such that the Dirichlet boundary conditions are
@@ -91,7 +92,10 @@
 # %% [markdown]
 # ### Exercise 3
 #
-# Adjust the code below to plot the solution $u_{\mathrm{exact}}$ for a fixed value of $c$ such that the boundary conditions are satisfied. Extend the code to also plot the $f$ you derived previously. Add axis labels and enable a legend for the graph.
+# Adjust the code below to plot the solution $u_{\mathrm{exact}}$ for a fixed
+# value of $c$ such that the boundary conditions are satisfied. Extend the code
+# to also plot the $f$ you derived previously. Add axis labels and enable a
+# legend for the graph.
 
 # %%
 import numpy as np
@@ -109,9 +113,11 @@ plt.plot(x, u_exact)
 # %% [markdown]
 # ### Exercise 5
 #
-# Derive the weak formulation of the partial differential equation. You will need the following integration by parts result.
+# Derive the weak formulation of the partial differential equation. You will
+# need the following integration by parts result.
 #
-# For functions $u, v \in H^1(\Omega)$ the following integration by parts result holds
+# For functions $u, v \in H^1(\Omega)$ the following integration by parts
+# result holds
 #
 # $$
 # \int_{0}^{1} -u'' v \; \mathrm{d}x = [-u' v]_0^1 + \int_0^1 u' v' \; \mathrm{d}x.
@@ -129,7 +135,8 @@ plt.plot(x, u_exact)
 #
 # The mesh is a discretisation of the domain $\Omega$ into cells.
 #
-# A uniform mesh $\mathcal{T}_h$ of $\Omega$ is a partitioning of the mesh constructed from $N$ disjoint cells $K_i$
+# A uniform mesh $\mathcal{T}_h$ of $\Omega$ is a partitioning of the mesh
+# constructed from $N$ disjoint cells $K_i$
 #
 # $$
 # \mathcal{T}_h = \{ K_i = (x_i, x_{i+1}) \; | \; i = 0, 1, \dots, N-1 \},
@@ -182,7 +189,8 @@ plt.plot(x, u_exact)
 #
 # ### Nodal basis property
 #
-# The standard hat functions $\{ \varphi_j \}_{j=0}^N$ form a basis for $V_h$. Each basis function $\varphi_j$ has the property
+# The standard hat functions $\{ \varphi_j \}_{j=0}^N$ form a basis for $V_h$.
+# Each basis function $\varphi_j$ has the property
 #
 # $$
 # \varphi_j(x_i) = \delta_{ij} = \begin{cases}
@@ -196,11 +204,20 @@ plt.plot(x, u_exact)
 #
 # In the next cell we show a plot of the finite element basis functions on an adjustable mesh.
 #
-# Begin by dragging the slider to $N = 2$. The endpoints of the single cell $K_0$ are shown by the grey vertical lines at $x = 0$ and $x = 1$. The coloured circles along the lower axis denote the location of the two *degrees of freedom* $x_j$ associated with the basis functions $\varphi_j$. The two linear basis functions, along with each associated degree of freedom, are plotted in the same colour.
+# Begin by dragging the slider to $N = 2$. The endpoints of the single cell
+# $K_0$ are shown by the grey vertical lines at $x = 0$ and $x = 1$. The
+# coloured circles along the lower axis denote the location of the two *degrees
+# of freedom* $x_j$ associated with the basis functions $\varphi_j$. The two
+# linear basis functions, along with each associated degree of freedom, are
+# plotted in the same colour.
 #
-# > The $N = 2$ case is similar to the situation when we construct the local finite element basis functions later on in the course, and then pull back the integral from each global cell $K_j = (x_j, x_{j + 1})$ to the local cell $\hat{K} = (0, 1)$.
+# > The $N = 2$ case is similar to the situation when we construct the local
+# finite element basis functions later on in the course, and then pull back the
+# integral from each global cell $K_j = (x_j, x_{j + 1})$ to the local cell
+# $\hat{K} = (0, 1)$.
 #
-# Experiment with increasing $N$. Match up what you see in the plot with the equations and properties discussed in the previous paragraphs.
+# Experiment with increasing $N$. Match up what you see in the plot with the
+# equations and properties discussed in the previous paragraphs.
 
 # %%
 interact(plot_p1_basis, N=(2, 10))
@@ -217,16 +234,26 @@ interact(plot_p1_basis, N=(2, 10))
 #
 # where $u(x_j)$ are the nodal values of $u$.
 #
-# > The nodal values of the $P^1$ space are associated with the vertices of the mesh. In this course we do not pay any particular care to the association between the topological entities of the mesh (e.g. vertices) and the nodes (or degrees of freedom) of the finite element space $V_h$. We also do not strictly separate the topology of the mesh from its geometry. These precisions will be made in the Numerical Methods for Variational Problems course in Semester 3.
+# > The nodal values of the $P^1$ space are associated with the vertices of the
+# > mesh. In this course we do not pay any particular care to the association
+# > between the topological entities of the mesh (e.g. vertices) and the nodes
+# > (or degrees of freedom) of the finite element space $V_h$. We also do not
+# > strictly separate the topology of the mesh from its geometry. These
+# > precisions will be made in the Numerical Methods for Variational Problems
+# > course in Semester 3.
 #
 # The interpolation operator has the following properties:
 #
-# 1. Exact (to machine precision) reproduction of linear functions, i.e. if $u$ is linear, then $I_h u = u$ everywhere.
+# 1. Exact (to machine precision) reproduction of linear functions, i.e. if $u$
+# is linear, then $I_h u = u$ everywhere.
 # 2. $I_h u$ is $C^0$ on $\Omega$.
-# 3. The basis is local in a spatial sense - the value of $(I_h u)(x)$ depends only on $u(x_j)$ at the nearest nodes.
-# 4. The construction of $\varphi_j$ ensures that the interpolant $I_h u$ matches $u$ at the vertices, i.e. $(I_h u)(x_j) = u(x_j)$.
+# 3. The basis is local in a spatial sense - the value of $(I_h u)(x)$ depends
+#    only on $u(x_j)$ at the nearest nodes.
+# 4. The construction of $\varphi_j$ ensures that the interpolant $I_h u$
+#    matches $u$ at the vertices, i.e. $(I_h u)(x_j) = u(x_j)$.
 #
-# The interpolant of $u$ can be written in terms of the two local basis functions
+# The interpolant of $u$ can be written in terms of the two local basis
+# functions
 #
 # $$
 # I_h u(x) = u(x_i)\varphi_i(x) + u(x_{i+1})\varphi_{i+1}(x),
@@ -241,13 +268,16 @@ interact(plot_p1_basis, N=(2, 10))
 # %% [markdown]
 # ### Exercise 6
 #
-# The interpolation error estimate states that the error between the exact solution $u_{\mathrm{exact}} := u \in H^2$ and its interpolant $I_h u \in V_h$ is given by
+# The interpolation error estimate states that the error between the exact
+# solution $u_{\mathrm{exact}} := u \in H^2$ and its interpolant $I_h u \in
+# V_h$ is given by
 #
 # $$
 # \lVert u - I_h u \rVert_{H^1} \leq C h | u |_{H^2}
 # $$
 #
 # where $\lVert \cdot \rVert_{H^1}^2$ is the standard $ H^1 $ norm
+#
 # $$
 # \lVert v \rVert_{H^1} = \lVert v \rVert_{L^2}^2 + \lVert \nabla v \rVert_{L^2}^2,
 # $$
@@ -256,13 +286,23 @@ interact(plot_p1_basis, N=(2, 10))
 #     | u |_{H^2} = \left( \int_0^1 | u''(x) |^2 dx \right)^{1/2}.
 # $$
 #
-# For the exact solution given above, derive an explicit bound for the interpolation error in terms of the parameters $C$, $h$, and $c$. In words, comment on the dependence between the parameters and the error, and give an intepretation of the result.
+# For the exact solution given above, derive an explicit bound for the
+# interpolation error in terms of the parameters $C$, $h$, and $c$. In words,
+# comment on the dependence between the parameters and the error, and give an
+# intepretation of the result.
 #
-# > The extended version of the previous result, called a finite element error bound, will be covered in the theoretical part of the course. The finite element error bound states that the error between the exact solution and the finite element solution $u_h$ is given by
+# > The extended version of the previous result, called a finite element error
+# > bound, will be covered in the theoretical part of the course. The finite
+# > element error bound states that the error between the exact solution and the
+# > linear Lagrange finite element solution $u_h$ is given by
+#
 # $$
 # \lVert u - u_h \rVert_{H^1} \leq C h | u |_{H^2}
 # $$
-# > which is of a very similar form to the interpolation error estimate we just saw. Note that $C$ is a different positive constant to the one you have just seen - this applies throughout the course!
+#
+# > which is of a very similar form to the interpolation error estimate we just
+# > saw. Note that $C$ is a different positive constant to the one you have just
+# > seen - this applies throughout the course!
 
 # %% [markdown]
 # *Answer*
@@ -272,11 +312,16 @@ interact(plot_p1_basis, N=(2, 10))
 # %% [markdown]
 # ### Exercise 7
 #
-# Having seen the interpolation error estimate is is helpful to play around with the parameters $c$, controlling how oscillatory our solution $u$ is, and $h = 1/N$ and simply observe how the interpolant $I_h u$ changes.
+# Having seen the interpolation error estimate is is helpful to play around
+# with the parameters $c$, controlling how oscillatory our solution $u$ is, and
+# $h = 1/N$ and simply observe how the interpolant $I_h u$ changes.
 #
-# Matplotlib already implements the linear interpolant; you give it an array of values $(I_h u)(x_j)$ of the function $u$ at the nodes $x_j$ and it will plot the linear interpolant $I_h u$.
+# Matplotlib already implements the linear interpolant; you give it an array of
+# values $(I_h u)(x_j)$ of the function $u$ at the nodes $x_j$ and it will plot
+# the linear interpolant $I_h u$.
 #
-# In the box below the plot explain your observations and relate them back to the interpolation error estimate we just saw.
+# In the box below the plot explain your observations and relate them back to
+# the interpolation error estimate we just saw.
 
 
 # %%
